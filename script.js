a9c56ec73ec3c028a119860458dcb58b065a6e18
MYuPopov <beemax@bk.ru>
2022-01-10 01:41:30 +0300

    Выполнена вёрстка

--- script.js
+++ script.js
@@ -7,6 +7,17 @@ const button = document.querySelector('.button');
 const input2 = document.querySelector('.input2');
 const span2 = document.querySelector('.span2');
 
+const sendData = () => {
+	fetch('https://api.exchangeratesapi.io/v1/latest?access_key=API_KEY')
+		.then((res) => res.json())
+		.then((data) => {
+			console.log(data);
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+};
+
 selectBlock.addEventListener('change', (e) => {
 	if (e.target.value === '0') {
 		input1.setAttribute('disabled', '');
@@ -41,22 +52,26 @@ input1.addEventListener('blur', (e) => {
 	e.target.value = e.target.value.replace(/[-\s]+$/, '');
 });
 
-// const data = fetch('db.json')
-// 	.then((res) => res.json())
-// 	.then((cars) => {
-// 		selectBlock.addEventListener('change', (e) => {
-// 			renderText(cars, e.target.value);
-// 		});
-// 	})
-// 	.catch((error) => {
-// 		console.log(error);
-// 	});
+button.addEventListener('click', (e) => {
+	e.preventDefault();
+	sendData();
+});
 
-// const renderText = (cars, item) => {
-// 	if (item != 0) {
-// 		text.innerHTML = `Тачка: <b>${cars[item - 1].brand} ${cars[item - 1].model}</b><br>
-// 	      Цена: <b>${cars[item - 1].price}$</b>`;
-// 	} else {
-// 		text.innerHTML = 'Выберите тачку';
-// 	}
+// fetch('https://api.exchangeratesapi.io/v1/latest?access_key=API_KEY&fromEUR&toUSD&amount=10')
+
+// const sendData = (date) => {
+// 	fetch('https://api.exchangeratesapi.io/v1/latest', {
+// 		method: 'POST',
+// 		body: JSON.stringify(data),
+// 		headers: {
+// 			'Content-type': 'application/json; charset=UTF-8',
+// 		},
+// 	})
+// 		.then((res) => res.json())
+// 		.then((data) => {
+// 			console.log(data);
+// 		})
+// 		.catch((error) => {
+// 			console.log(error);
+// 		});
 // };
